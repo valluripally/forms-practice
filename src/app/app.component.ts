da3c91c04684a9e78308b8315e120cae2a3c72fc
saishree <Saishree@leadwinner.com>
2022-09-16 12:21:49 +0530

    using viewchild and css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  @ViewChild('f') signUpForm :NgForm
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
-  onSubmit(f:NgForm){
-    console.log(f);
+  // onSubmit(f:NgForm){
+  //   console.log(f);
+  // }
+
+  onSubmit(){
+    console.log(this.signUpForm);
   }
 }
